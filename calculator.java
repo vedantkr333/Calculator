bae82c5862dcda70ad015dc1912dd27117d9b88a
Vedant Kumar <vedantkumar3333@gmail.com>
2025-09-22 21:49:12 +0530

    completing this code

--- calculator.java
+++ calculator.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
 public class calculator {
-    
-}
+   public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+            System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Enter operation (+, -, *, /): ");
+        char op = scanner.next().charAt(0);
+                double result;
+        switch (op) {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/':
+                if (num2 != 0) result = num1 / num2;
+                else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+        System.out.println("Result: " + result);
+    }
+
+   } 
+
